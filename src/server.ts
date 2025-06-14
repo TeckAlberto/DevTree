71490a34634c25911559ea34d6bfb73c69ee855c
Jesus Alberto Gutierrez Lizaola <jes.alberto.ing@gmail.com>
2025-06-13 23:46:05 -0600

    adjust server for cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express, {Express} from 'express';
+import cors from 'cors';
 import 'dotenv/config'
 import router from './router';
 import { connectDB } from './config/db';
 
-const server : Express = express();
 connectDB();
+
+const server : Express = express();
+
+// Cors
+server.use(cors())
 
 // Read form's data
 server.use(express.json());
